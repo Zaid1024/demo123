386a763817259d4b5eae0f58bd1bdffb668a30b8
Zaid1024 <zaidhussain0124@gmail.com>
2024-07-19 20:29:07 +0530

    initial commits

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function sayHello(name) {
 }
 
 sayHello("John");
+
+function findFactorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return number * findFactorial(number - 1);
+  }
+}
+
+const number = 5;
+const factorial = findFactorial(number);
+console.log(`Factorial of ${number} is: ${factorial}`);
